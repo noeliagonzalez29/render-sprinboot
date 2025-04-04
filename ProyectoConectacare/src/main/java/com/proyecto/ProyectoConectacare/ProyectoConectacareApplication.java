ec313bc1b03ab3aa81622f908b59cdda523c6d9f
noeliagonzalez29 <ngonzalezg31@educarex.es>
2025-04-04 17:34:16 +0200

    empezando

---
+++ ProyectoConectacare/src/main/java/com/proyecto/ProyectoConectacare/ProyectoConectacareApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.ProyectoConectacare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoConectacareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoConectacareApplication.class, args);
+	}
+
+}
---
+++ ProyectoConectacare/src/test/java/com/proyecto/ProyectoConectacare/ProyectoConectacareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.ProyectoConectacare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoConectacareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
